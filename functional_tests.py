bc2c44be7fb07d81a992ad973e06a9d9093ae152
FelipeHoch <hochfelipe@gmail.com>
2022-02-27 10:14:34 -0300

    Testes funcionais agora verifica o input da lista To-do

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,8 @@
 import unittest
+import time
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys 
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -17,21 +19,35 @@ class NewVisitorTest(unittest.TestCase):
 
         # Ela percebe quue o título da página e o cabeçalho mencionam listas de tarefas (to-do)
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # Ela é convidada a inserir um item de tarefa imediatamente
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            input_box.get_attribute('placeholder'),
+            'Enter a to-do item'
+            )
+
+        # Ela digita "Comprar penas de pavão" em uma caixa de texto "o hooby de Bruna é fazer iscas para pescas com Felipe"
+        input_box.send_keys('Comprar penas de pavão')
+
+        # Quando ela tecla enter, a página é atualizada, e agora a página lista "1: Compre penas de pavão" como um item em uma
+        # lista de tarefas
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Compre penas de pavão')
+            )
+
         self.fail('Finish the test!')
 
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
-
-
-# Ela é convidada a inserir um item de tarefa imediatamente
-
-
-# Ela digita "Comprar penas de pavão" em uma caixa de texto "o hooby de Bruna é fazer iscas para pescas com Felipe"
-
-
-# Quando ela tecla enter, a página é atualizada, e agora a página lista "1: Compre penas de pavão" como um item em uma
-# lista de tarefas
 
 
 # Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro item. Ela insere "Use penas de pavão"

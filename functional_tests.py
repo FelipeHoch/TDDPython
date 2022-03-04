6e67558c5e90a40e6256f869178bf1cdde633a6f
FelipeHoch <hochfelipe@gmail.com>
2022-03-04 13:03:12 -0300

    FT agora adiciona um input na tabela

--- functional_tests.py
+++ functional_tests.py
@@ -23,45 +23,50 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', header_text)
 
         # Ela é convidada a inserir um item de tarefa imediatamente
-        input_box = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-            input_box.get_attribute('placeholder'),
+            inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
 
         # Ela digita "Comprar penas de pavão" em uma caixa de texto "o hooby de Bruna é fazer iscas para pescas com Felipe"
-        input_box.send_keys('Comprar penas de pavão')
+        inputbox.send_keys('Comprar penas de pavão')
 
         # Quando ela tecla enter, a página é atualizada, e agora a página lista "1: Compre penas de pavão" como um item em uma
         # lista de tarefas
-        input_box.send_keys(Keys.ENTER)
+        inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
-        row = table.find_elements_by_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Compre penas de pavão'),
-            "New to-do item did not appear in table"
-        )
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Comprar penas de pavão', [row.text for row in rows])  
 
+        # Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro item. Ela insere "Use penas de pavão"
+        # para fazer um fly - Bruna é bem metódica
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Usar penas de pavão para fazer um fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+
+        # A página é atualizada novamente e agora mostra os dois itens em sua lista
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertIn('1: Comprar penas de pavão', [row.text for row in rows])
+        self.assertIn('2: Usar penas de pavão para fazer um fly',
+            [row.text for row in rows])
+
+
+        # Bruna se pergunta se o site lembrará de sua lista. Então ela nota que o site gerou uma URL único para ela -- há um
+        # pequeno texto explicativo para isso.
         self.fail('Finish the test!')
+
+
+        # Ela acessa esse URL - sua lista de tarefas continua lá.
 
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
 
-
-# Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro item. Ela insere "Use penas de pavão"
-# para fazer um fly - Bruna é bem metódica
-
-
-# A página é atualizada novamente e agora mostra os dois itens em sua lista
-
-
-# Bruna se pergunta se o site lembrará de sua lista. Então ela nota que o site gerou uma URL único para ela -- há um
-# pequeno texto explicativo para isso.
-
-
-# Ela acessa esse URL - sua lista de tarefas continua lá.
 
 # Satisfeita, ela volta a dormir
